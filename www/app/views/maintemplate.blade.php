44c2a1988b296d186443bfd8640174b5f6f736b7
Pekka <nospam@astudios.org>
2014-11-29 18:21:33 +0200

    Stuff lel

--- www/app/views/maintemplate.blade.php
+++ www/app/views/maintemplate.blade.php
@@ -3,6 +3,9 @@
 <head>
 	<meta charset='utf-8'>
 	<title>{{ $title }}</title>
+	<link rel='stylesheet' href='{{ URL::to('assets/css/all.min.css') }}'/>
+	<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+	<script src='{{ URL::to('assets/js/all.min.js') }}'></script>
 </head>
 <body>
 @include('home')
---
+++ www/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+
+var paths = {
+    scripts : ['assets/js/**/*.js'],
+    scss : 'assets/scss/**/*.scss'
+};
+
+gulp.task('scripts', function () {
+    return gulp.src(paths.scripts)
+        .pipe(uglify())
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('public/assets/js'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(cssmin())
+        .pipe(concat('all.min.css'))
+        .pipe(gulp.dest('public/assets/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.scss, ['styles']);
+});
+
+gulp.task('default', ['scripts', 'styles', 'watch']);
