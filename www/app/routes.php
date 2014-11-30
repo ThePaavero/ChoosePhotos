17180e7ecd6921b486595fa48e00c2a1275db812
ThePaavero <nospam@astudios.org>
2014-11-30 03:28:27 +0200

    Basic auth implemented

--- www/app/routes.php
+++ www/app/routes.php
@@ -1,5 +1,9 @@
 <?php
 
-Route::get('/', 'HomeController@index');
+Route::group(array ('before' => 'auth.basic'), function ()
+{
+    Route::get('/', 'HomeController@index');
+});
+
 Route::get('/project/{slug?}', 'ProjectController@project');
 Route::post('/project/{slug}/update-status', 'ProjectController@updatePictureStatus');
